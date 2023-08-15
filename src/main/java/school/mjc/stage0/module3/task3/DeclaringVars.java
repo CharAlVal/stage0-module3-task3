b099a6a8a736abc8061d213d3cfb73dc7ccb3230
Martín Alcalá <carlos_a_v_bm@hotmail.com>
2023-08-15 16:55:38 -0600

    Changed variable printed 1°ex

--- src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
+++ src/main/java/school/mjc/stage0/module3/task3/DeclaringVars.java
@@ -3,6 +3,6 @@ package school.mjc.stage0.module3.task3;
 public class DeclaringVars {
     public static void main(String[] args) {
         int a,b,c; a = b = c = 10;
-        System.out.println(a);
+        System.out.println(c);
     }
 }
